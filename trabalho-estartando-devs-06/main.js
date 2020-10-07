15afe803a5d704f3f1c2a4a39c00d1abb6d0d9a4
danilo Lima Dias <danilo743377@gmail.com>
2020-10-07 16:20:27 -0300

    exercicio de javascript do curso estartando devs

---
+++ trabalho-estartando-devs-06/main.js
@@ -0,0 +1,25 @@
+// 1 - Para passar no Colegio "Crias do Devs" os alunos precisam de media 8. Crie um código que tenha uma variavel com o valor da media do aluno no semestre retorne true ou false de acordo com o resultado do aluno no colegio.
+const mediaColegio = 8
+const notaAluno = 8
+
+console.log(notaAluno >= mediaColegio)
+
+// 2 - Verifique se as afirmativas estão certas, caso contrario justifique com uma prova real. Dica: Use o typeof para as provas reais.
+
+// A) O tipo da constante `typeNumber` é number.
+const typeNumber = "10";
+console.log(typeof typeNumber)
+
+// B) O valor da constante `y` é 149.
+const y = 147 + "2";
+console.log(y)
+
+
+// C) O tipo da constante `x` é number.
+const x = 13 > 20;
+console.log(typeof x)
+
+
+// D) O valor da constante `s` é "Estartando Devs".
+const s = "Estartando " + "Devs";
+console.log(s)
